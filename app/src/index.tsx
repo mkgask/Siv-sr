1114016cf45c1169c0d1e987039a9163c098abd0
mkgask <mmp@zsw.jp>
2022-10-15 23:34:35 +0900

    [readme] update

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -10,9 +10,9 @@ const container = document.getElementById("app")
 const root = createRoot(container)
 
 root.render(
-        <I18nextProvider i18n={i18n}>
-            <Suspense fallback="loading">
-                <Root store={store} history={history}></Root>
-            </Suspense>
-        </I18nextProvider>
+    <I18nextProvider i18n={i18n}>
+        <Suspense fallback="loading">
+            <Root store={store} history={history}></Root>
+        </Suspense>
+    </I18nextProvider>
 )
